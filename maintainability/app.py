a801f60ddd29c75c4d3ebed3aa80589865f32365
voynow <voynow99@gmail.com>
2023-09-12 20:12:25 -0400

    Updated config, new write function

--- maintainability/app.py
+++ maintainability/app.py
@@ -12,12 +12,11 @@ logging.basicConfig(level=logging.INFO)
 
 
 def analyze_code(response: str) -> Optional[Dict]:
-    print(response)
     jsonified_data = json.loads(response)
     return MaintainabilityMetrics(**jsonified_data)
 
 
-def analyze_maintainability(repo: Dict[Path, str]) -> Dict[Path, Dict]:
+def analyze_maintainability(repo: Dict[str, str]) -> Dict[str, Dict]:
     model_name = "gpt-3.5-turbo-16k"
     block = block_factory.get(
         "template", template=config["prompt"], temperature=0.0, model_name=model_name
@@ -25,7 +24,7 @@ def analyze_maintainability(repo: Dict[Path, str]) -> Dict[Path, Dict]:
 
     result = {}
     for filepath, code in repo.items():
-        if len(code):
+        if len(code.splitlines()) > config["min_num_lines"]:
             logging.info(f"Analyzing {filepath}")
             response = block(filepath=filepath, code=code)
             result[filepath] = analyze_code(response)
@@ -33,9 +32,14 @@ def analyze_maintainability(repo: Dict[Path, str]) -> Dict[Path, Dict]:
     return result
 
 
-def generate_output(maintainability_metrics: Dict[Path, Dict]) -> None:
-    with open("output.json", "w") as file:
-        json.dump(maintainability_metrics, file, indent=4)
+def write_output(maintainability_metrics: Dict[str, MaintainabilityMetrics]) -> None:
+    aggregated_metrics = {
+        filepath: metrics.__dict__
+        for filepath, metrics in maintainability_metrics.items()
+    }
+
+    with open("metrics.json", "w") as f:
+        json.dump(aggregated_metrics, f, indent=4)
 
 
 def main() -> None:
@@ -43,7 +47,7 @@ def main() -> None:
     repo = collect_text_from_files(Path("."), pathspec)
 
     maintainability_metrics = analyze_maintainability(repo)
-    generate_output(maintainability_metrics)
+    write_output(maintainability_metrics)
 
 
 if __name__ == "__main__":

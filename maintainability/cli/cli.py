c084a15761ae2d1514c8f640fc79202c7aa23dbb
voynow <voynow99@gmail.com>
2023-09-18 17:04:55 -0400

    Migrating code to a common module

--- maintainability/cli/cli.py
+++ maintainability/cli/cli.py
@@ -1,8 +1,11 @@
+import logging
+import uuid
 from pathlib import Path
+from typing import List
 
 import click
 
-from . import main
+from maintainability.common import config, utils
 
 options = {
     "multiple": True,
@@ -12,10 +15,26 @@ options = {
 }
 
 
+def main(paths: List[Path]) -> None:
+    filtered_repo = utils.filter_repo_by_paths(paths)
+    session_id = str(uuid.uuid4())
+
+    composite_metrics = {}
+    for filepath, code in filtered_repo.items():
+        if len(code.splitlines()) > config.MIN_NUM_LINES:
+            logging.info(f"Processing {filepath}")
+            composite_metrics[filepath] = utils.compose_metrics(
+                filepath, code, session_id
+            )
+
+    response = utils.write_metrics(composite_metrics)
+    logging.info(f"Response: {response}")
+
+
 @click.command()
 @click.option("--paths", **options)
 def cli(paths):
-    main.main([Path(path) for path in paths])
+    main([Path(path) for path in paths])
 
 
 if __name__ == "__main__":

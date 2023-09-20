63d5eca52964dba230d45d87009e83444326a744
voynow <voynow99@gmail.com>
2023-09-19 21:32:16 -0400

    Shifting file operations away form API

--- maintainability/api/src/main.py
+++ maintainability/api/src/main.py
@@ -5,7 +5,7 @@ from typing import Dict
 
 from fastapi import FastAPI, HTTPException
 
-from . import config, models, utils
+from . import config, metrics_manager, models
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -19,18 +19,18 @@ def read_root():
 
 @app.post("/submit_metrics")
 async def submit_metrics(metrics: Dict[str, models.CompositeMetrics]):
-    utils.write_metrics(metrics)
+    metrics_manager.write_metrics(metrics)
     return {"status": "ok", "message": "Metrics submitted successfully."}
 
 
 @app.post("/extract_metrics")
 async def extract_metrics(repo: Dict[str, str]):
     session_id = str(uuid.uuid4())
-    composite_metrics: Dict[str, utils.CompositeMetrics] = {}
+    composite_metrics: Dict[str, metrics_manager.CompositeMetrics] = {}
     try:
         for filepath, code in repo.items():
             if len(code.splitlines()) > config.MIN_NUM_LINES:
-                composite_metrics[filepath] = utils.compose_metrics(
+                composite_metrics[filepath] = metrics_manager.compose_metrics(
                     Path(filepath), code, session_id
                 )
     except Exception as e:

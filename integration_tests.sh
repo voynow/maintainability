aa31bef85195fc538e447289f622eaee7dbb14b9
voynow <voynow99@gmail.com>
2023-09-29 18:40:41 -0400

    Trying to get tests to fail

--- integration_tests.sh
+++ integration_tests.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# Exit on error
+set -e
+
 # Set the API_URL environment variable to the local API server
 export API_URL=http://localhost:8000
 
 # Change to the maintainability directory
 cd maintainability/
+ls
 
 # Start the API server in the background
 uvicorn api.src.main:app --port 8000 &

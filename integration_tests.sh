e8d4662131c404fae9860cbc0f0a6e90c8a1508b
voynow <voynow99@gmail.com>
2023-09-29 18:43:16 -0400

    Validating cwd understanding in action

--- integration_tests.sh
+++ integration_tests.sh
@@ -6,9 +6,9 @@ set -e
 # Set the API_URL environment variable to the local API server
 export API_URL=http://localhost:8000
 
-# Change to the maintainability directory
-cd maintainability/
+echo "ls output:"
 ls
+echo "done ls"
 
 # Start the API server in the background
 uvicorn api.src.main:app --port 8000 &

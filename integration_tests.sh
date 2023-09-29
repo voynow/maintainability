fdc15023ae9f430ed114bb1d40738416e1621539
voynow <voynow99@gmail.com>
2023-09-29 18:50:01 -0400

    Debugging actions

--- integration_tests.sh
+++ integration_tests.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+# Function to clean up background process
+cleanup() {
+  kill -9 $API_PID
+}
+
+# Register cleanup function to run on script exit
+trap cleanup EXIT
+
 # Exit on error
 set -e
 
 # Set the API_URL environment variable to the local API server
 export API_URL=http://localhost:8000
-
-echo "ls output:"
-ls
-echo "done ls"
 
 # Start the API server in the background
 uvicorn maintainability.api.src.main:app --port 8000 &
@@ -17,6 +21,3 @@ sleep 5
 
 # Run the integration tests
 pytest
-
-# Kill the API server
-kill -9 $API_PID

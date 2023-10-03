20d85b1f6a0c3c205abc5f30bc637d4f40ef9acf
voynow <voynow99@gmail.com>
2023-10-03 14:51:34 -0400

    Improving API logging

--- maintainability/api/src/main.py
+++ maintainability/api/src/main.py
@@ -3,9 +3,9 @@ import uuid
 from pathlib import Path
 from typing import Dict
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 
-from . import config, metrics_manager, models
+from . import config, io_operations, metrics_manager, models
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -19,19 +19,18 @@ def read_root():
 
 @app.post("/submit_metrics")
 async def submit_metrics(metrics: Dict[str, models.CompositeMetrics]):
-    metrics_manager.write_metrics(metrics)
+    io_operations.write_metrics(metrics)
     return {"status": "ok", "message": "Metrics submitted successfully."}
 
 
 @app.post("/extract_metrics")
 async def extract_metrics(repo: Dict[str, str]):
     session_id = str(uuid.uuid4())
-    composite_metrics: Dict[str, metrics_manager.CompositeMetrics] = {}
+    composite_metrics: Dict[str, models.CompositeMetrics] = {}
     try:
-        print("Inside /extract_metrics endpoint")
         for filepath, code in repo.items():
             if len(code.splitlines()) > config.MIN_NUM_LINES:
-                print(f"Composing metrics for {filepath}")
+                io_operations.logger(f"Processing {filepath}..."
                 composite_metrics[filepath] = metrics_manager.compose_metrics(
                     Path(filepath), code, session_id
                 )
--- maintainability/api/src/metrics_manager.py
+++ maintainability/api/src/metrics_manager.py
@@ -1,38 +1,31 @@
 import json
-import os
-import uuid
 from datetime import datetime
 from pathlib import Path
-from typing import Dict, Tuple
 
 from llm_blocks import block_factory
-from supabase import Client, create_client
 
-from . import config, models
+from . import config, io_operations, models
 
 
 def get_maintainability_metrics(
     filepath: Path, code: str
 ) -> models.MaintainabilityMetrics:
-    print("metrics_manager.py: get_maintainability_metrics()")
     llm = block_factory.get(
         "template",
         template=config.PROMPT,
         temperature=config.TEMPERATURE,
         model_name=config.MODEL_NAME,
     )
-    print("metrics_manager.py: get_maintainability_metrics() Calling llm()")
+    io_operations.logger(f"LLM request: filepath={filepath}, code={code}")
     response = llm(filepath=filepath, code=code)
-    print("metrics_manager.py: get_maintainability_metrics() llm() returned")
+    io_operations.logger(f"LLM response: {response}")
     return models.MaintainabilityMetrics(**json.loads(response))
 
 
 def get_file_metrics(filepath: Path, content: str) -> models.FileMetrics:
-    print("metrics_manager.py: get_file_metrics()")
     file_size = len(content.encode("utf-8"))
     language = filepath.suffix.lstrip(".")
     loc = len(content.splitlines())
-    print("metrics_manager.py: get_file_metrics() returning")
     return models.FileMetrics(
         file_size=file_size, loc=loc, language=language, content=content
     )
@@ -41,44 +34,11 @@ def get_file_metrics(filepath: Path, content: str) -> models.FileMetrics:
 def compose_metrics(
     filepath: Path, code: str, session_id: str
 ) -> models.CompositeMetrics:
-    print("metrics_manager.py: compose_metrics()")
     maintainability_metrics = get_maintainability_metrics(filepath, code)
     file_metrics = get_file_metrics(filepath, code)
-    print("metrics_manager.py: compose_metrics() returning")
     return models.CompositeMetrics(
         maintainability=maintainability_metrics,
         file_info=file_metrics,
         timestamp=datetime.utcnow().isoformat(),
         session_id=session_id,
     )
-
-
-def connect_to_supabase() -> Client:
-    """Connect to Supabase database"""
-    return create_client(
-        os.environ.get("SUPABASE_URL"),
-        os.environ.get("SUPABASE_KEY"),
-    )
-
-
-def write_metrics(metrics: Dict[str, models.CompositeMetrics]) -> Tuple:
-    insert_data = [
-        {
-            "primary_id": str(uuid.uuid4()),
-            "file_path": filepath,
-            "readability": metrics.maintainability.readability,
-            "design_quality": metrics.maintainability.design_quality,
-            "testability": metrics.maintainability.testability,
-            "consistency": metrics.maintainability.consistency,
-            "debug_error_handling": metrics.maintainability.debug_error_handling,
-            "file_size": metrics.file_info.file_size,
-            "language": metrics.file_info.language,
-            "loc": metrics.file_info.loc,
-            "content": metrics.file_info.content,
-            "timestamp": metrics.timestamp,
-            "session_id": metrics.session_id,
-        }
-        for filepath, metrics in metrics.items()
-    ]
-    table = connect_to_supabase().table("maintainability")
-    return table.insert(insert_data).execute()

867e2f0ae644d5d74ef7ad5da1994fab7e7d0740
voynow <voynow99@gmail.com>
2023-10-06 20:29:28 -0400

    Cleanup unused components

--- webapp/src/components/common/Button.js
+++
@@ -1,9 +0,0 @@
-const Button = ({ label, onClick, className }) => {
-    return (
-        <button onClick={onClick} className={`p-3 ${className}`}>
-            {label}
-        </button>
-    );
-};
-
-export default Button;

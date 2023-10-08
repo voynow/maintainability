dd4dc344354ce6fb5b380cc7ed025ac7fb80190d
voynow <voynow99@gmail.com>
2023-10-08 17:35:38 -0400

    Common error handling

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -4,21 +4,24 @@ import Main from './components/Main';
 import Register from './components/Register';
 import { useAppContext } from './AppContext';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import ErrorBoundary from './ErrorBoundary';
 
 
 const App = () => {
     const { isLoggedIn } = useAppContext();
 
     return (
-        <div className="h-screen">
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/login" element={!isLoggedIn ? <Login /> : <Navigate to="/" />} />
-                    <Route path="/register" element={!isLoggedIn ? <Register /> : <Navigate to="/" />} />
-                    <Route path="*" element={isLoggedIn ? <Main /> : <Navigate to="/login" />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
+        <ErrorBoundary>
+            <div className="h-screen">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/login" element={!isLoggedIn ? <Login /> : <Navigate to="/" />} />
+                        <Route path="/register" element={!isLoggedIn ? <Register /> : <Navigate to="/" />} />
+                        <Route path="*" element={isLoggedIn ? <Main /> : <Navigate to="/login" />} />
+                    </Routes>
+                </BrowserRouter>
+            </div>
+        </ErrorBoundary>
     );
 };
 

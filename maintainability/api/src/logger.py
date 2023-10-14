587678f678eceb6a5ca794eecea7583b01219c6e
voynow <voynow99@gmail.com>
2023-10-13 20:55:06 -0400

    Update logging format

--- maintainability/api/src/logger.py
+++ maintainability/api/src/logger.py
@@ -5,7 +5,7 @@ from . import io_operations
 def write_log_to_supabase(frame, message):
     filename = frame.f_code.co_filename
     lineno = frame.f_lineno
-    loc = f"File: {filename}, Line: {lineno}"
+    loc = f"{filename}:{lineno}"
     io_operations.write_log(loc, message)
 
 

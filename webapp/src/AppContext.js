00b16215fd49c62313797d38b52d6badb40d78ec
voynow <voynow99@gmail.com>
2023-10-29 18:10:19 -0400

    Trying to shift logic from Login to appcontext for simplicity

--- webapp/src/AppContext.js
+++ webapp/src/AppContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
+import supabase from './supabaseClient';
 
 const AppContext = createContext();
 
@@ -8,25 +9,32 @@ export const useAppContext = () => {
 
 export const AppProvider = ({ children }) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [currentPage, setCurrentPage] = useState('Analytics');
     const [email, setEmail] = useState(null);
-    const [selectedProject, setSelectedProject] = useState(null);
 
-    const logout = () => {
-        localStorage.removeItem('access_token');
-        setIsLoggedIn(false);
+    useEffect(() => {
+        const session = supabase.auth.session();
+        setIsLoggedIn(!!session);
+        if (session) {
+            setEmail(session.user.email);
+        }
+    }, []);
+
+    const logout = async () => {
+        const { error } = await supabase.auth.signOut();
+        if (error) {
+            console.error("Error during logout:", error);
+        } else {
+            setIsLoggedIn(false);
+            setEmail(null);
+        }
     };
 
     const value = {
         isLoggedIn,
         setIsLoggedIn,
-        currentPage,
-        setCurrentPage,
         email,
         setEmail,
-        logout,
-        selectedProject,
-        setSelectedProject
+        logout
     };
 
     return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
--- webapp/src/components/Login.js
+++ webapp/src/components/Login.js
@@ -1,26 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import supabase from '../supabaseClient';
 import { useAppContext } from '../AppContext';
 
 function Login() {
-    const [user, setUser] = useState(null);
-    const { setIsLoggedIn, setEmail: setGlobalEmail } = useAppContext();
+    const { setIsLoggedIn, setEmail, isLoggedIn } = useAppContext();
 
     useEffect(() => {
-        checkUser();
-        window.addEventListener('hashchange', function () {
-            checkUser();
-        });
-    }, []);
-
-    async function checkUser() {
         const user = supabase.auth.user();
-        setUser(user);
         setIsLoggedIn(!!user);
         if (user) {
-            setGlobalEmail(user.email);
+            setEmail(user.email);
         }
-    }
+    }, []);
 
     async function signInWithGithub() {
         await supabase.auth.signIn({
@@ -28,30 +19,16 @@ function Login() {
         });
     }
 
-    async function signOut() {
-        await supabase.auth.signOut();
-        setUser(null);
-        setIsLoggedIn(false);
-        setGlobalEmail(null);
-    }
-
     return (
         <div className="h-screen bg-gray-100 flex items-center justify-center">
-            {user ? (
-                <div className="bg-white p-6 rounded-lg shadow-lg w-1/4 text-center">
-                    <h1 className="text-2xl font-semibold mb-4">Hello, {user.email}</h1>
-                    <button onClick={signOut} className="bg-red-500 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded">
-                        Sign out
-                    </button>
-                </div>
-            ) : (
+            {!isLoggedIn ? (
                 <div className="bg-white p-6 rounded-lg shadow-lg w-1/4 text-center">
                     <h1 className="text-2xl font-semibold mb-4">Hello, please sign in!</h1>
                     <button onClick={signInWithGithub} className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded">
                         Sign In
                     </button>
                 </div>
-            )}
+            ) : null}
         </div>
     );
 }

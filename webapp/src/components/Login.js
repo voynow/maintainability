ac500c370894fbe055e44bf3e776b2ab388d0e38
voynow <voynow99@gmail.com>
2023-10-30 08:31:19 -0400

    Need to add login maybe

--- webapp/src/components/Login.js
+++ webapp/src/components/Login.js
@@ -1,9 +1,11 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import supabase from '../supabaseClient';
 import { useAppContext } from '../AppContext';
 
 function Login() {
     const { setIsLoggedIn, setEmail, isLoggedIn } = useAppContext();
+    const [email, setInputEmail] = useState('');
+    const [password, setInputPassword] = useState('');
 
     useEffect(() => {
         const user = supabase.auth.user();
@@ -13,10 +15,17 @@ function Login() {
         }
     }, []);
 
-    async function signInWithGithub() {
-        await supabase.auth.signIn({
-            provider: 'github'
+    async function signInWithEmail() {
+        const { data, error } = await supabase.auth.signIn({
+            email,
+            password,
         });
+
+        if (error) {
+            console.error("Error during sign-in:", error);
+        } else {
+            console.log("Signed in:", data);
+        }
     }
 
     return (
@@ -24,7 +33,21 @@ function Login() {
             {!isLoggedIn ? (
                 <div className="bg-white p-6 rounded-lg shadow-lg w-1/4 text-center">
                     <h1 className="text-2xl font-semibold mb-4">Hello, please sign in!</h1>
-                    <button onClick={signInWithGithub} className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded">
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setInputEmail(e.target.value)}
+                        className="mb-2 p-1 border rounded"
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setInputPassword(e.target.value)}
+                        className="mb-2 p-1 border rounded"
+                    />
+                    <button onClick={signInWithEmail} className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded">
                         Sign In
                     </button>
                 </div>

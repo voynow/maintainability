6f2b1e5a2fccc3a455fd11b0390954a088d9cca7
voynow <voynow99@gmail.com>
2023-11-05 14:46:09 -0500

    Landing page MVP

--- webapp/src/components/LandingPage.js
+++ webapp/src/components/LandingPage.js
@@ -1,25 +1,43 @@
-// webapp/src/components/LandingPage.js
-
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const LandingPage = () => {
-    let navigate = useNavigate();
+    const navigate = useNavigate();
 
-    const navigateToLogin = () => {
-        navigate('/login');
+    const handleNavigate = (path) => {
+        navigate(path);
     };
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-4xl font-bold mb-4">Welcome to Maintainability</h1>
-            <button
-                onClick={navigateToLogin}
-                className="px-6 py-2 border rounded text-xl font-medium hover:bg-gray-200 transition duration-300"
-            >
-                Log In
-            </button>
-        </div>
+        <main className="text-gray-700">
+            <section className="flex flex-col items-center justify-center min-h-screen p-4">
+                <div className="max-w-4xl mx-auto text-center">
+                    <h1 className="text-6xl font-bold mb-6">Maintainability</h1>
+                    <p className="text-xl mb-8">
+                        Streamline your projects and enhance your productivity. Discover the new age of project management with Maintainability.
+                    </p>
+                    <div className="flex justify-center gap-4">
+                        <button
+                            onClick={() => handleNavigate('/signup')}
+                            className="px-8 py-3 bg-blue-600 text-white text-lg font-semibold rounded-lg hover:bg-blue-700 transition duration-300"
+                        >
+                            Get Started
+                        </button>
+                        <button
+                            onClick={() => handleNavigate('/login')}
+                            className="px-8 py-3 border border-blue-600 text-blue-600 text-lg font-semibold rounded-lg hover:bg-blue-50 transition duration-300"
+                        >
+                            Log In
+                        </button>
+                    </div>
+                </div>
+            </section>
+            <section className="w-full flex justify-center px-4 py-8 -mt-60">
+                <div className="shadow-2xl rounded-lg overflow-hidden">
+                    <img src="/landing_page.png" alt="Workflow" className="max-w-5xl w-full h-auto" />
+                </div>
+            </section>
+        </main>
     );
 };
 

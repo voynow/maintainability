026a50b423e2413f150fe3f7544b3fe63add9b54
voynow <voynow99@gmail.com>
2023-11-10 18:24:53 -0500

    Updating token name according to github policy

--- maintainability/api/src/github.py
+++ maintainability/api/src/github.py
@@ -7,10 +7,12 @@ from . import config, logger
 
 router = APIRouter()
 
+GH_AUTH_TOKEN = os.environ.get("GH_AUTH_TOKEN")
+
 
 @router.get("/fetch_repo_structure")
 def fetch_repo_structure(user: str, repo: str, branch: str = "main") -> list:
-    headers = {"Authorization": f"token {os.environ.get('GITHUB_AUTH_TOKEN')}"}
+    headers = {"Authorization": f"token {GH_AUTH_TOKEN}"}
     url = f"https://api.github.com/repos/{user}/{repo}/git/trees/{branch}?recursive=1"
 
     resp = requests.get(url, headers=headers)
@@ -26,7 +28,7 @@ def fetch_repo_structure(user: str, repo: str, branch: str = "main") -> list:
 
 @router.get("/fetch_file_content")
 def fetch_file_content(user: str, repo: str, path: str) -> str:
-    headers = {"Authorization": f"token {os.environ.get('GITHUB_AUTH_TOKEN')}"}
+    headers = {"Authorization": f"token {GH_AUTH_TOKEN}"}
     file_url = f"https://api.github.com/repos/{user}/{repo}/contents/{path}"
     file_resp = requests.get(file_url, headers=headers)
     file_resp.raise_for_status()

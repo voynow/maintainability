c95dab10c69cc42a9723ec2d8e011f5bc54a50d6
voynow <voynow99@gmail.com>
2023-11-10 15:50:45 -0500

    Adding get github endpoints

---
+++ maintainability/api/src/github.py
@@ -0,0 +1,46 @@
+import os
+import requests
+import base64
+from fastapi import APIRouter
+
+from . import config
+
+router = APIRouter()
+
+
+@router.get("/fetch_repo_structure")
+def fetch_repo_structure(user: str, repo: str, branch: str = "main") -> list:
+    headers = {"Authorization": f"token {os.environ.get('GITHUB_AUTH_TOKEN')}"}
+    tree_api_url = (
+        f"https://api.github.com/repos/{user}/{repo}/git/trees/{branch}?recursive=1"
+    )
+
+    resp = requests.get(tree_api_url, headers=headers)
+    resp.raise_for_status()
+
+    return [
+        f["path"]
+        for f in resp.json().get("tree", [])
+        if f["type"] == "blob"
+        and any(f["path"].endswith(ext) for ext in config.EXTENSIONS)
+    ]
+
+
+@router.get("/fetch_repo_structure")
+def fetch_file_content(file_url: str) -> str:
+    headers = {"Authorization": f"token {os.environ.get('GITHUB_AUTH_TOKEN')}"}
+    file_resp = requests.get(file_url, headers=headers)
+    file_resp.raise_for_status()
+    return base64.b64decode(file_resp.json()["content"]).decode("utf-8")
+
+
+#
+# file_paths = fetch_repo_structure(user, repo)
+
+# repodata = {}
+# for path in file_paths:
+#     file_url = f"https://api.github.com/repos/{user}/{repo}/contents/{path}"
+#     file_content = fetch_file_content(file_url, headers)
+#     repodata[path] = file_content
+
+# return repodata

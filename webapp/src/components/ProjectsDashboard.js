3f3ae5189b684a627f12727f5d8c4e92a9d08615
voynow <voynow99@gmail.com>
2023-11-11 16:52:30 -0500

    Selected project checkmark

--- webapp/src/components/ProjectsDashboard.js
+++ webapp/src/components/ProjectsDashboard.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { Dialog, DialogTitle, List, ListItem, ListItemText } from '@mui/material';
 import { useAppContext } from '../AppContext';
+import CheckIcon from '@mui/icons-material/Check';
 
 const ProjectsDashboard = ({ open, onClose }) => {
-    const { projects, setSelectedProject } = useAppContext();
-    console.log(projects);
+    const { projects, selectedProject, setSelectedProject } = useAppContext();
 
     return (
         <Dialog onClose={onClose} open={open}>
             <DialogTitle>Choose a Project</DialogTitle>
             <List>
-                {projects && projects.map((project, index) => (
-                    <ListItem button onClick={() => {
-                        setSelectedProject(project.project_name);
-                        onClose();
-                    }} key={index}>
+                {projects.map((project, index) => (
+                    <ListItem
+                        button
+                        onClick={() => {
+                            setSelectedProject(project.project_name);
+                            onClose();
+                        }}
+                        key={index}
+                        selected={selectedProject === project.project_name}
+                    >
                         <ListItemText primary={project.project_name} />
+                        {selectedProject === project.project_name && <CheckIcon />}
                     </ListItem>
                 ))}
             </List>

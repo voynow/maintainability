e8200b8925e07b088f6e05815fe0300aea9251fb
voynow <voynow99@gmail.com>
2023-11-12 12:59:53 -0500

    Aligning projects dashboard with profile menu

--- webapp/src/components/ProjectsDashboard.js
+++ webapp/src/components/ProjectsDashboard.js
@@ -1,30 +1,50 @@
 import React from 'react';
-import { Dialog, DialogTitle, List, ListItem, ListItemText } from '@mui/material';
+import { Dialog, DialogTitle, DialogContent, List, ListItem, ListItemText, Accordion, AccordionSummary, AccordionDetails, Tooltip } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AssessmentIcon from '@mui/icons-material/Assessment';
 import { useAppContext } from '../AppContext';
-import CheckIcon from '@mui/icons-material/Check';
 
 const ProjectsDashboard = ({ open, onClose }) => {
-    const { projects, selectedProject, setSelectedProject } = useAppContext();
+    const { email, projects, selectedProject, setSelectedProject } = useAppContext();
+
+    const handleSelectProject = (projectName) => {
+        setSelectedProject(projectName);
+        onClose();
+    };
 
     return (
-        <Dialog onClose={onClose} open={open}>
-            <DialogTitle>Choose a Project</DialogTitle>
-            <List>
-                {projects.map((project, index) => (
-                    <ListItem
-                        button
-                        onClick={() => {
-                            setSelectedProject(project.project_name);
-                            onClose();
-                        }}
-                        key={index}
-                        selected={selectedProject === project.project_name}
-                    >
-                        <ListItemText primary={project.project_name} />
-                        {selectedProject === project.project_name && <CheckIcon />}
-                    </ListItem>
-                ))}
-            </List>
+        <Dialog onClose={onClose} open={open} fullWidth maxWidth="md">
+            <DialogTitle>
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    <AssessmentIcon sx={{ marginRight: '4px', fontSize: '30px', color: '#CD5C5C' }} />
+                    <span>{email}'s projects</span>
+                </div>
+            </DialogTitle>
+            <DialogContent>
+                <List>
+                    {projects.map(project => (
+                        <ListItem
+                            button
+                            onClick={() => handleSelectProject(project.project_name)}
+                            selected={selectedProject === project.project_name}
+                            style={{ cursor: 'pointer' }}
+                        >
+                            {selectedProject === project.project_name}
+                            <ListItemText primary={project.project_name} />
+                            <Accordion>
+                                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                                    <Tooltip title="Project Details">
+                                        <span>Details</span>
+                                    </Tooltip>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    {/* Project specific details */}
+                                </AccordionDetails>
+                            </Accordion>
+                        </ListItem>
+                    ))}
+                </List>
+            </DialogContent>
         </Dialog>
     );
 };

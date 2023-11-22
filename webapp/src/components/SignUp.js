776af01e775a63aae703418cbe4c396bcf19684d
voynow <voynow99@gmail.com>
2023-11-22 14:07:56 -0500

    Fixing up singup

--- webapp/src/components/SignUp.js
+++ webapp/src/components/SignUp.js
@@ -28,36 +28,54 @@ function SignUp() {
             {emailSent ? (
                 <p className="text-center">A verification email has been sent. Please check your inbox.</p>
             ) : (
-                <>
-                    <h1 className="text-4xl font-bold mb-4">Sign Up</h1>
-                    <input
-                        type="email"
-                        placeholder="Email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        className="mb-4 p-2 border-2 rounded w-64 bg-transparent"
-                    />
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        className="mb-4 p-2 border-2 rounded w-64 bg-transparent"
-                    />
+                <div className="rounded-lg p-8 shadow-2xl bg-white w-96">
+                    <h1 className="text-4xl font-bold mb-8">Sign Up</h1>
+                    <div className="mb-4 relative">
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+                            <svg className="h-5 w-5 text-gray-500" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
+                                <path d="M16 17l-4 4m0 0l-4-4m4 4V3"></path>
+                            </svg>
+                        </span>
+                        <input
+                            type="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="pl-10 pr-3 py-2 border rounded w-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+                        />
+                    </div>
+                    <div className="mb-6 relative">
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+                            <svg className="h-5 w-5 text-gray-500" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
+                                <path d="M19 9l-7 7-7-7"></path>
+                            </svg>
+                        </span>
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="pl-10 pr-3 py-2 border rounded w-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+                        />
+                    </div>
                     <button
                         onClick={signUpWithEmail}
-                        className="px-6 py-2 border-2 rounded text-xl font-medium hover:bg-gray-200 transition duration-300 w-64"
+                        className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50 transition duration-300"
                     >
                         Sign Up
                     </button>
-                    <p onClick={() => navigate('/login')} className="cursor-pointer text-blue-600 hover:underline mt-4">
+                    <p
+                        onClick={() => navigate('/login')}
+                        className="text-center text-blue-600 hover:underline mt-4 cursor-pointer"
+                    >
                         Already have an account? Log In
                     </p>
                     {errorMessage && <p className="text-red-500 mt-2">{errorMessage}</p>}
-                </>
+                </div>
             )}
         </div>
     );
 }
 
 export default SignUp;
+

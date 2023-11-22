ef5f80e6979f0d055d06cdf2025c96ab0d1934bc
voynow <voynow99@gmail.com>
2023-11-22 11:14:00 -0500

    Spacing updates & New metric blurbs

--- webapp/src/components/LandingPage.js
+++ webapp/src/components/LandingPage.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import SpeedIcon from '@mui/icons-material/Speed';
+import CodeIcon from '@mui/icons-material/Code';
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import InsightsIcon from '@mui/icons-material/Insights';
+import LayersIcon from '@mui/icons-material/Layers';
 
 const LandingPage = () => {
     const navigate = useNavigate();
 
+    const metrics = [
+        {
+            title: 'Intuitive Design',
+            summary: 'Streamlined user interfaces that enhance user experience and engagement.',
+            icon: <InsightsIcon />,
+        },
+        {
+            title: 'Functional Cohesion',
+            summary: 'Highly cohesive code modules that improve maintainability and scalability.',
+            icon: <LayersIcon />,
+        },
+        {
+            title: 'Performance Efficiency',
+            summary: 'Optimized code for the fastest execution and responsiveness.',
+            icon: <SpeedIcon />,
+        },
+        {
+            title: 'Code Quality',
+            summary: 'Adherence to best coding practices and standards.',
+            icon: <CodeIcon />,
+        },
+        {
+            title: 'Growth Potential',
+            summary: 'Scalability and adaptability of the codebase for future growth.',
+            icon: <TrendingUpIcon />,
+        }
+    ];
+
     return (
         <main className="text-gray-700">
-            <section className="flex flex-col items-center justify-center min-h-screen p-4">
+            <section className="flex flex-col items-center justify-center min-h-screen p-4 -mt-16">
                 <div className="max-w-4xl mx-auto text-center">
                     <h1 className="text-8xl mb-2 font-bold">
                         Maint<span className="text-red-500">AI</span>nability
@@ -31,20 +64,22 @@ const LandingPage = () => {
                     </div>
                 </div>
             </section>
-            <section className="w-full flex justify-center px-4 pt-8 -mt-40 relative pb-32">
+            <section className="w-full flex justify-center px-4 pt-8 -mt-32 relative pb-32">
                 <div className="relative shadow-2xl rounded-lg">
                     <img src="/landing_page.png" alt="Workflow" className="max-w-5xl w-full h-auto" />
-                    <div className="absolute bottom-0 translate-y-1/2 transform -translate-x-1/2 left-1/2 bg-gray-100 text-gray-700 px-8 py-4 rounded-lg shadow-lg w-full max-w-lg text-left">
-                        <h3 className="text-xl font-bold">Example Insights</h3>
-                        <p className="text-md mt-1">
-                            Out of the box your projects will be evaluated against:
-                        </p>
-                        <ul className="list-disc pl-6 mt-1 text-md">
-                            <li>Intuitive design</li>
-                            <li>Functional cohesion</li>
-                            <li>And more</li>
-                        </ul>
-                    </div>
+                </div>
+            </section>
+            <section className="flex justify-center p-4">
+                <div className="flex flex-row flex-wrap justify-center gap-4">
+                    {metrics.map((metric, index) => (
+                        <div key={index} className="flex-none bg-white border-l-4 border-blue-500 shadow-md p-4">
+                            <div className="flex items-center mb-2">
+                                {metric.icon}
+                                <h3 className="ml-2 text-lg font-semibold">{metric.title}</h3>
+                            </div>
+                            <p className="text-sm text-gray-600">{metric.summary}</p>
+                        </div>
+                    ))}
                 </div>
             </section>
             <footer className="text-center p-4 text-gray-800">

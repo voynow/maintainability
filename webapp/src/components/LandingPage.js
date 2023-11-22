7b67ed93e09ee1368b01ca31ebee7b500b8d83bf
voynow <voynow99@gmail.com>
2023-11-22 13:42:14 -0500

    Cleanup, testing again

--- webapp/src/components/LandingPage.js
+++ webapp/src/components/LandingPage.js
@@ -40,50 +40,62 @@ const LandingPage = () => {
 
     return (
         <main className="text-gray-700">
-            <section className="flex flex-col items-center justify-center min-h-screen p-4 -mt-16">
+            <section className="w-full flex justify-center px-4 pt-8 relative pb-32 mt-32">
                 <div className="max-w-4xl mx-auto text-center">
                     <h1 className="text-8xl mb-2 font-bold">
                         Maint<span className="text-red-500">AI</span>nability
                     </h1>
-                    <h1 className="text-6xl font-bold text-blue-300">The Future of Code Analysis</h1>
+                    <h1 className="text-6xl font-bold text-blue-400">The Future of Code Analysis</h1>
                     <p className='mt-4 mb-4 text-xl'>
                         Leveraging advanced Language Models, our platform offers a nuanced understanding of code that goes beyond traditional static code analysis systems.
                     </p>
                     <div className="flex justify-center gap-4">
                         <button
                             onClick={() => navigate('/signup')}
-                            className="px-8 py-3 border-2 border-gray-300 text-lg text-lg font-semibold rounded-lg hover:bg-gray-100 transition duration-300"
+                            className="px-8 py-3 border-2 bg-white border-gray-400 text-lg text-lg font-semibold rounded-lg hover:bg-blue-100 transition duration-300"
                         >
                             Get Started
                         </button>
                         <button
                             onClick={() => navigate('/login')}
-                            className="px-8 py-3 border-2 border-gray-300 text-lg text-lg font-semibold rounded-lg hover:bg-gray-100 transition duration-300"
+                            className="px-8 py-3 border-2 bg-white border-gray-400 text-lg text-lg font-semibold rounded-lg hover:bg-blue-100 transition duration-300"
                         >
                             Log In
                         </button>
                     </div>
                 </div>
             </section>
-            <section className="w-full flex justify-center px-4 pt-8 -mt-32 relative pb-32">
+            <section className="w-full flex justify-center px-4 pt-8 relative">
                 <div className="relative shadow-2xl rounded-lg">
                     <img src="/landing_page.png" alt="Workflow" className="max-w-5xl w-full h-auto" />
                 </div>
             </section>
-            <section className="flex justify-center p-4 mb-16">
-                <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 justify-items-center gap-4 max-w-7xl mx-auto">
-                    {metrics.map((metric, index) => (
-                        <div key={index} className="bg-white border-l-4 border-blue-500 shadow-md p-4 rounded-lg flex flex-col items-center">
-                            <div className="flex items-center mb-2">
-                                {metric.icon}
-                                <h3 className="ml-2 text-lg font-semibold">{metric.title}</h3>
+            <section className="flex justify-center p-4 mb-24 mt-32">
+                <div className="flex flex-col items-center">
+                    <div className="flex justify-center gap-4 mb-4">
+                        {metrics.slice(0, 3).map((metric, index) => (
+                            <div key={index} className="bg-white border-l-4 border-blue-400 shadow-md p-4 rounded-lg flex flex-col items-center" style={{ width: '300px', height: '200px' }}>
+                                <div className="flex items-center mb-2">
+                                    {metric.icon}
+                                    <h3 className="ml-2 text-lg font-semibold">{metric.title}</h3>
+                                </div>
+                                <p className="text-sm text-gray-600">{metric.summary}</p>
                             </div>
-                            <p className="text-sm text-gray-600">{metric.summary}</p>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
+                    <div className="flex justify-center gap-4">
+                        {metrics.slice(3, 5).map((metric, index) => (
+                            <div key={index} className="bg-white border-l-4 border-blue-400 shadow-md p-4 rounded-lg flex flex-col items-center" style={{ width: '300px', height: '200px' }}>
+                                <div className="flex items-center mb-2">
+                                    {metric.icon}
+                                    <h3 className="ml-2 text-lg font-semibold">{metric.title}</h3>
+                                </div>
+                                <p className="text-sm text-gray-600">{metric.summary}</p>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </section>
-
             <footer className="text-center p-4 text-gray-800">
                 <p className="text-sm">
                     &copy; {new Date().getFullYear()} Maintainability. All rights reserved.<br></br>Contact me at voynow99@gmail.com or connect with me on X @jamievoynow

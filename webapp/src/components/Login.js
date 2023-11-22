31464ea1710b8b9d94822d61d2b5764048934bdf
voynow <voynow99@gmail.com>
2023-11-22 14:01:07 -0500

    Fixing up login

--- webapp/src/components/Login.js
+++ webapp/src/components/Login.js
@@ -18,37 +18,56 @@ function Login() {
             setErrorMessage(error.message || 'An error occurred during sign-in.');
             console.error("Error during sign-in:", error);
         } else {
-            navigate('/'); // Navigate to the main page upon successful login
+            navigate('/');
         }
     }
 
     return (
         <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-4xl font-bold mb-4">Login</h1>
-            <input
-                type="email"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="mb-4 p-2 border-2 rounded w-64 bg-transparent"
-            />
-            <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="mb-4 p-2 border-2 rounded w-64 bg-transparent"
-            />
-            <button
-                onClick={signInWithEmail}
-                className="px-6 py-2 border-2 rounded text-xl font-medium hover:bg-gray-200 transition duration-300 w-64"
-            >
-                Sign In
-            </button>
-            <p onClick={() => navigate('/signup')} className="cursor-pointer text-blue-600 hover:underline mt-4">
-                Don't have an account? Sign Up
-            </p>
-            {errorMessage && <p className="text-red-500 mt-2">{errorMessage}</p>}
+            <div className="rounded-lg p-8 shadow-2xl bg-white w-96">
+                <h1 className="text-4xl font-bold mb-8">Login</h1>
+                <div className="mb-4 relative">
+                    <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+                        <svg className="h-5 w-5 text-gray-500" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
+                            <path d="M16 17l-4 4m0 0l-4-4m4 4V3"></path>
+                        </svg>
+                    </span>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="pl-10 pr-3 py-2 border rounded w-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+                    />
+                </div>
+                <div className="mb-6 relative">
+                    <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+                        <svg className="h-5 w-5 text-gray-500" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
+                            <path d="M19 9l-7 7-7-7"></path>
+                        </svg>
+                    </span>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        className="pl-10 pr-3 py-2 border rounded w-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+                    />
+                </div>
+                <button
+                    onClick={signInWithEmail}
+                    className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50 transition duration-300"
+                >
+                    Sign In
+                </button>
+                <p
+                    onClick={() => navigate('/signup')}
+                    className="text-center text-blue-600 hover:underline mt-4 cursor-pointer"
+                >
+                    Don't have an account? Sign Up
+                </p>
+                {errorMessage && <p className="text-red-500 mt-2">{errorMessage}</p>}
+            </div>
         </div>
     );
 }
